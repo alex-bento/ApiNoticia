c79d097220bf4f6600bb9f57a378e5aa37b1c51c
Alex Bento <melo1bento@gmail.com>
2021-10-19 22:20:36 -0300

    Adicionando as blibiotecas, e colocando as colunas para fazer a migração

---
+++ API/Entidades/Noticia/Notifica.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+ namespace Entidades.Noticia
+{
+    public class Notifica
+    {
+        // Aqui Vamos Criar a nosa tabela com o Banco
+
+        public Notifica()
+        {
+            Notificacoes = new List<Notifica>();
+        }
+
+        [NotMapped]
+        public string NomePropriedade { get; set; }
+
+        [NotMapped]
+        public string Mensagem { get; set; }
+
+        [NotMapped]
+        public List<Notifica> Notificacoes;
+
+        public bool ValidarPropriedaeString(string valor, string nomePropriedade)
+        {
+
+            if (string.IsNullOrWhiteSpace(valor) || string.IsNullOrWhiteSpace(nomePropriedade))
+            {
+                Notificacoes.Add(new Notifica
+                {
+                    Mensagem = "Campo Obrigatorio",
+                    NomePropriedade = nomePropriedade
+                });
+
+                return false;
+            }
+
+            return true;
+        }
+
+        public bool ValidarPropriedaeDecimal(decimal valor, string nomePropriedade)
+        {
+
+            if (valor < 1 || string.IsNullOrWhiteSpace(nomePropriedade))
+            {
+                Notificacoes.Add(new Notifica
+                {
+                    Mensagem = "O Valor deve ser maior que 0",
+                    NomePropriedade = nomePropriedade
+                });
+
+                return false;
+            }
+
+            return true;
+        }
+
+    }
+}
